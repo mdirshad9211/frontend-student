6ac3827351b0c23e501191669de624de735e63ca
Md Irshad <irshadidrisi9211@gmail.com>
2026-07-12 12:38:44 +0530

    added fixes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,7 @@ import toast from 'react-hot-toast'
 const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
 
 export const api = axios.create({
-  baseURL: `${baseURL}/api`,
+  baseURL: `${baseURL}`,
   timeout: 20000,
 })
 
